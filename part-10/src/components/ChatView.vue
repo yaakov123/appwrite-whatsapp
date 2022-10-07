5ac4590b374fb0fae4d10a8d50a7db8ba9b05170
Jacob <beckermanyaakov@gmail.com>
2022-10-07 12:41:05 +0300

    part 10

---
+++ part-10/src/components/ChatView.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="h-full w-full">
+    <header class="w-full bg-gray-100 h-[55px] flex items-center px-2">
+      <Avatar />
+      <div class="ml-5">{{ groupStore.currentGroup?.name }}</div>
+    </header>
+    <ChatHistory></ChatHistory>
+    <ChatInput></ChatInput>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { useGroupStore } from "../stores/groupStore";
+import Avatar from "./Avatar.vue";
+import ChatHistory from "./ChatHistory.vue";
+import ChatInput from "./ChatInput.vue";
+const groupStore = useGroupStore();
+</script>
